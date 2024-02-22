7f0654f7996f36e6ca5ebb41fba15f35df37e32f
JavierPalacios1723 <javierpalacios257@gmail.com>
2024-02-21 23:31:06 -0300

    cambios en archivos

--- controllers/authentication/login.controller.js
+++ controllers/authentication/login.controller.js
@@ -3,3 +3,8 @@ const path = require ('path');
 module.exports = (req, res) => {
     res.send('rrrrrrr')    /*prueba de funcionamiento*/ 
 }
+
+
+/*module.exports = (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/productCart.html'))
+}*/
--- controllers/authentication/register.controller.js
+++ controllers/authentication/register.controller.js
@@ -3,3 +3,7 @@ const path = require ('path');
 module.exports = (req, res) => {
     res.send('jojsodosos')   /*prueba de funcionamiento*/ 
     }
+
+    /*module.exports = (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views/productDetail.html'))
+}*/
